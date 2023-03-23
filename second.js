9af48040b54ed669ac2e166f5b57c1fa06a9c879
mahesh-57 <111737667+mahesh-57@users.noreply.github.com>
2023-03-23 15:28:31 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,21 @@
+function add(x,y){
+    return x+y;
+} 
+
+function sub(x,y){
+    return x-y;
+} 
+
+function mul(x,y){
+    return x*y;
+}
+
+function div(x,y){
+    return x/y;
+}
+
+exports.add = add;                                                                                                                                                                                                           
+exports.sub = sub;
+exports.mul = mul;
+exports.div = div;
+
